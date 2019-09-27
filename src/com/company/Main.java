1251424b758b30965ccc136acb70178d4d288cbd
Rahul kumar <rahulkumar96369@gmail.com>
2019-09-27 18:02:22 +0530

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Enter the input Sentence");
+        String c;
+
+        Scanner obj =new Scanner(System.in);
+
+        c =obj.nextLine();
+
+        System.out.println(  "The output Sentence is: " +c);
+
+
+    }
+}
